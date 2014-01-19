1bfaa215ba35815e7bb4dd8e292e2f696342fa1d
Sum567 <scsumeetchandra@gmail.com>
2014-01-19 16:03:42 -0700

    Drive for a specifies amount of time.

--- org/bitbuckets/frc2014/commands/DriveMotorTime.java
+++ org/bitbuckets/frc2014/commands/DriveMotorTime.java
@@ -1,28 +1,43 @@
 
 package org.bitbuckets.frc2014.commands;
 
+import edu.wpi.first.wpilibj.SpeedController;
+import java.util.Calendar;
+import java.util.TimeZone;
+
 /**
  *
  * @author bradmiller
  */
-public class ExampleCommand extends CommandBase {
 
-    public ExampleCommand() {
+public class DriveMotorTime extends CommandBase {
+    
+    private int time;
+    private int speed;
+    private SpeedController controlSpeed;
+    private long initTime;
+    
+    public DriveMotorTime(int time, int speed, SpeedController controlSpeed) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+        this.time = time;
+        this.controlSpeed= controlSpeed;
+        this.speed = speed;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        initTime = Calendar.getInstance(TimeZone.getDefault()).getTime().getTime();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        controlSpeed.set(speed);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return Calendar.getInstance(TimeZone.getDefault()).getTime().getTime() - initTime >= time? true: false;
     }
 
     // Called once after isFinished returns true
