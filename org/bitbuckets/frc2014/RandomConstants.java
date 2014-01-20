fcaf738ced724dc5ad49dcb3112d99e1569b5189
bitbuckets4183 <the.bit.buckets@gmail.com>
2014-01-19 17:30:12 -0700

    Added a limiter for the drivetrain. Took way too long.

---
+++ org/bitbuckets/frc2014/RandomConstants.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.bitbuckets.frc2014;
+
+/**
+ *
+ * @author admin
+ */
+public class RandomConstants {
+    
+    public static final double maxMagChange = .5;
+    public static final double maxCurChange = .5;
+    
+}
--- org/bitbuckets/frc2014/subsystems/DriveTrain.java
+++ org/bitbuckets/frc2014/subsystems/DriveTrain.java
@@ -3,6 +3,7 @@ package org.bitbuckets.frc2014.subsystems;
 
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.bitbuckets.frc2014.RandomConstants;
 import org.bitbuckets.frc2014.RobotMap;
 
 /**
@@ -12,6 +13,8 @@ public class DriveTrain extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     public RobotDrive drive;
+    double magnitudeBefore = 1;
+    double curveBefore = 1;
     
     public DriveTrain() {
         super();
@@ -24,7 +27,24 @@ public class DriveTrain extends Subsystem {
     }
     
     public void drive(double outputMagnitude, double curve){
-        drive.arcadeDrive(outputMagnitude, -curve);
+        double dMag = outputMagnitude - magnitudeBefore;
+        double dCur = curve - curveBefore;
+        int signMag = 1;
+        int signCur = 1;
+        
+        if(dMag < 1)
+            signMag = -1;
+        if(dCur < 1)
+            signCur = -1;
+        
+        if(Math.abs(dMag) > RandomConstants.maxMagChange)
+            dMag = RandomConstants.maxMagChange*signCur;
+        if(Math.abs(dCur) > RandomConstants.maxCurChange)
+            dCur = RandomConstants.maxCurChange*signCur;
+        
+        drive.arcadeDrive(magnitudeBefore + dMag, -(curveBefore+dCur));
+        magnitudeBefore = magnitudeBefore + dMag;
+        curveBefore = curveBefore + dCur;
     }
 }
 
