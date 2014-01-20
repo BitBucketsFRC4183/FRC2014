9c9388a3a6a5c721e238447b9f40de7a5ec97756
Sum567 <scsumeetchandra@gmail.com>
2014-01-19 17:38:04 -0700

    Documented things

--- org/bitbuckets/frc2014/RobotMap.java
+++ org/bitbuckets/frc2014/RobotMap.java
@@ -39,34 +39,34 @@ public class RobotMap {
      */
     public static final int INTAKE_LOWER_SOLENOID_R = 4;
     /**
-     * 
+     * The left solenoid that fires the catapult
      */
     public static final int CATAPAULT_FIRE_SOLENOID_L = 5;
     /**
-     * 
+     * The left solenoid that retracts the catapult
      */
     public static final int CATAPAULT_RETRACT_SOLENOID_L = 6;
     /**
-     * 
+     * The right solenoid that fires the catapult
      */
     public static final int CATAPAULT_FIRE_SOLENOID_R = 7;
     /**
-     * 
+     * The right catapult that retracts the catapult
      */
     public static final int CATAPAULT_RETRACT_SOLENOID_R = 8;
     
     /**
-     * 
+     * The relay the compressor is plugged in to
      */
     public static final int COMPRESSOR_RELAY = 4;
     
     /**
-     * 
+     * The switch that tells the compressor the max pressure
      */
     public static final int PRESSURE_SWITCH = 14;
     
     /**
-     * 
+     * The port the joystick is plugged in to
      */
     public static final int JOY_1_PORT = 1;
     
