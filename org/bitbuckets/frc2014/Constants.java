7cae839dd14f01eaece7cc0e422bcea85a060204
Kevin Forbes <forbesk9@gmail.com>
2014-01-29 00:49:19 -0700

    Reformed comments to match standards.

--- org/bitbuckets/frc2014/Constants.java
+++ org/bitbuckets/frc2014/Constants.java
@@ -1,3 +1,11 @@
+/*
+ * FRC 4183 - The Bit Buckets
+ * Tucson, AZ
+ *
+ * FRC 2014 Codebase
+ * Constants.java
+ */
+
 package org.bitbuckets.frc2014;
 
 import java.util.Hashtable;
@@ -8,7 +16,9 @@ import java.io.DataInputStream;
 
 
 /**
- * CSV constants class.
+ * Constants
+ * @author  Kevin Forbes
+ * 
  * Access and update constants on-the-fly in an external CSV file. Format the 
  * constants as follows:
  *      constant1,0.0
@@ -21,27 +31,35 @@ import java.io.DataInputStream;
  *      Constants.get("constant1");         //returns a double, 0.0
  *      Constants.get("intakeChannel");     //returns a double, 2.0
  *      Constants.getInt("intakeChannel");  //returns an int, 2
- * 
- * @author forbesk
  */
 public class Constants {
+    /** 
+     * vars
+     * Mapping of constant names as Strings to values as Doubles. 
+     * Using obsolete data structure due to Squawk limitations.
+     */
     private static Hashtable vars = new Hashtable();
     
+    /**
+    * Constants
+    * Empty private constructor for static class.
+    */
     private Constants() {
         
     }
     
-    /*
-    parseCVS
-    Reads a comma-separated values file and stores each line as a new constant.
-    Because a hash table is used, values are overwritten if parseCSV is called
-    more than once, allowing on-the-fly adjusting of constants.
-    The csv should be formatted as such:
-        <constantName>,<value>
-    where constantName is in lowerCamelCase and value is a double. Each constant
-    should be on its own line.
-    
-    Returns true if the operation was successful and false otherwise.
+    /**
+    * parseCSV
+    * Reads a comma-separated values file and stores each line as a new constant.
+    * Because a hash table is used, values are overwritten if parseCSV is called
+    * more than once, allowing on-the-fly adjusting of constants.
+    * The csv should be formatted as such:
+    *     <constantName>,<value>
+    * where constantName is in lowerCamelCase and value is a double. Each constant
+    * should be on its own line.
+    * 
+    * @param    filepath    location of file containing constant names and values
+    * @return               true on successful load, false otherwise
     */
     public static boolean parseCSV(String filepath) {
         FileConnection connection;
@@ -77,27 +95,33 @@ public class Constants {
         return true;
     }
     
-    /*
-    isSet
-    Returns true if the given constant name is loaded.
-    */
+    /**
+     * isSet
+     * Determines if the given name is present in the constants table.
+     * @param   name    constant name to look up
+     * @return          true if found, false otherwise
+     */
     public static boolean isSet(String name) {
         return vars.containsKey(name);
     }
     
-    /*
-    get
-    Returns the given constant's value as a double.
-    */
+    /**
+     * get
+     * Retrieves the stored value of the given constant name as a double
+     * @param   name    constant name to look up
+     * @return          value of constant as a double
+     */
     public static double get(String name) {
         Double value = (Double)vars.get(name);
         return value.doubleValue();
     }
     
-    /*
-    getInt
-    Returns the given constant's value as an integer.
-    */
+    /**
+     * getInt
+     * Retrieves the stored value of the given constant name as an integer
+     * @param   name    constant name to look up
+     * @return          value of constant as an integer
+     */
     public static double getInt(String name) {
         Double value = (Double)vars.get(name);
         return (int)value.doubleValue();
