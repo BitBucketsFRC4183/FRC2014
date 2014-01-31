323b8e05a5e58c79b7b30463417d223503f13532
bitbuckets4183 <the.bit.buckets@gmail.com>
2014-01-30 18:43:02 -0700

    Started editing the drivetrain class.

--- org/bitbuckets/frc2014/subsystems/DriveTrain.java
+++ org/bitbuckets/frc2014/subsystems/DriveTrain.java
@@ -16,63 +16,31 @@ public class DriveTrain extends Subsystem {
     private RobotDrive drive;
     private double throttle = 1;
     private double rotation = 1;
-    public Encoder encR;
-    public Encoder encL;
     
     public DriveTrain() {
         super();
         drive  = new RobotDrive(RobotMap.R_MOTOR, RobotMap.L_MOTOR);
-        encR = new Encoder(RobotMap.R_ENCODER_A, RobotMap.R_ENCODER_B);
-        encL = new Encoder(RobotMap.L_ENCODER_A, RobotMap.L_ENCODER_B);
     }
     
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+    
     /**
-     * Basic driving method.  Can be used in autonomous and teleop.
+     * Arcade driving method. Used in teleop.
+     * 
      * @param outputMagnitude
      * @param curve 
      */
     public void drive(double throttle, double rotation){
-        this.throttle = accelerationLimiter(this.throttle, throttle, RandomConstants.MAX_MAG_CHANGE);
-        this.rotation = accelerationLimiter(this.rotation, rotation, RandomConstants.MAX_CUR_CHANGE);
-        drive.arcadeDrive(this.throttle, -this.rotation);
+        throttle = accelerationLimiter(throttle, throttle, RandomConstants.MAX_MAG_CHANGE);
+        rotation = accelerationLimiter(rotation, rotation, RandomConstants.MAX_CUR_CHANGE);
+        drive.arcadeDrive(throttle, rotation);
     }
     
     /**
-     * More sophicsticated driving method for teleop.
-     * @param outputMagnitude
-     * @param curve 
-     */
-    public void cheesyDrive(double outputMagnitude, double curve) {
-        //See http://www.chiefdelphi.com/forums/showpost.php?p=1181728&postcount=2 for reference.
-        if(outputMagnitude >= RandomConstants.THROTTLE_CUTOFF)
-            curve *= (RandomConstants.TURN_GAIN * Math.abs(outputMagnitude));
-        
-        double tLeft = outputMagnitude + curve;
-        double tRight = outputMagnitude - curve;
-
-        drive.tankDrive(tLeft + skim(tRight), tRight + skim(tLeft));
-    }
-    
-    /**
-     * If drive speed for one side > 1, subtract some of the difference from other side's speed to maintain constant turning performance.
-     * Used in cheesyDrive.
-     * @param v
-     * @return 
-     */
-    public double skim(double v) {
-        if(v > 1.0) {
-            return -((v - 1.0) * RandomConstants.SKIM_GAIN);
-        } else if (v < -1.0)
-            return -((v + 1.0) * RandomConstants.SKIM_GAIN);
-        return 0;
-    }
-    
-    /**
-     * 
+     * Limits the acceleration of the motors so that they don't kill the clutch.
      * 
      * @param oldValue The value from the previous iteration.
      * @param requestedValue The value being requested to be changed to.
